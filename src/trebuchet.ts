863b18ddc2f1c202fd4eefe26e552fd39728fdb5
Stefan Boos <kontakt@boos.systems>
2023-12-01 08:16:18 +0100

    docs: problem understanding and solution approach

---
+++ src/trebuchet.ts
@@ -0,0 +1,40 @@
+// Understanding the problem
+// =========================
+//
+// assumption: every row contains 1 or more digits
+//
+// single digit is duplicated
+// 0 -> 00
+// 1 -> 11
+// 2 -> 22
+//
+// single digit mixed with letters is duplicated
+// a0 -> 00
+// aa0 -> 00
+// same for a1, a2, 1a, 2a, ... a0a, ...
+//
+// two digits are extracted to 2 digit number
+// 01 -> 01
+// 02 -> 02 ...
+//
+// 10 -> 10
+// ...
+//
+// for three or more digits, only the first and the last digits are used
+//
+// Tests from the problem description
+// 1abc2 -> 12
+// pqr3stu8vwx -> 38
+// a1b2c3d4e5f -> 15
+// treb7uchet -> 77
+//
+// Solution approach
+// -----------------
+//
+// trebuchet = For every row in the input:
+//   recover =
+//     Remove all letters
+//     identify the first digit
+//     identify the last digit
+//     combine digits to a string
+//     convert string to number
