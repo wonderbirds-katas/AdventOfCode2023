30bae294f4f6bad462af479f9a8bcedf4c692583
Stefan Boos <kontakt@boos.systems>
2023-12-02 17:32:03 +0100

    feat: templates allow setting up a new puzzle quickly.

---
+++ src/00-template.ts
@@ -0,0 +1,31 @@
+// Part 1 - Understanding the problem
+// ==================================
+//
+// ... list some simple tests showing how I understand the puzzle ...
+//
+// Tests from the problem description
+//
+// ... copy tests from problme description ...
+//
+// Solution approach
+// -----------------
+//
+// ... describe solution and algorithm idea roughly ...
+//
+// Part 2 - Understanding the problem
+// ==================================
+//
+// ... list some simple tests showing how I understand the puzzle ...
+//
+// Tests from the problem description
+//
+// ... copy tests from problme description ...
+//
+// Solution approach
+// -----------------
+//
+// ... describe solution and algorithm idea roughly ...
+//
+export function solution(input: string): number {
+  return 0;
+}
--- tests/failingTest.ts
+++
@@ -1,8 +0,0 @@
-import "chai/register-should";
-import { config } from "chai";
-config.truncateThreshold = 0;
-
-it('should fail', () => {
-    const actual = false;
-    actual.should.be.true;
-});
