62f08a1c13c0db1aecdb5bc079c03345227ac012
Stefan Boos <kontakt@boos.systems>
2023-12-03 21:19:16 +0100

    test: boolean matrix can be printed like a schematic

--- tests/gearRatios.test.ts
+++ tests/gearRatios.test.ts
@@ -2,6 +2,10 @@ import "chai/register-should";
 import { config } from "chai";
 import { gearRatios } from "../src/gearRatios";
 import { readFileSync } from "fs";
+import { beforeAll, describe, it } from "@jest/globals";
+import { JestReporter } from "approvals/lib/Providers/Jest/JestReporter";
+import { verify } from "approvals/lib/Providers/Jest/JestApprovals";
+import { configure } from "approvals";
 
 config.truncateThreshold = 0;
 
@@ -17,5 +21,32 @@ describe("gearRatios", () => {
       const input = readFileSync("./inputs/gearRatios.txt", "utf-8");
       gearRatios(input).should.equal(expected);
     });
+  });
+});
+
+describe("printBooleanMatrix", () => {
+  function printBooleanMatrix(booleanMatrix: boolean[][]) {
+    let output = "";
+    for (const row of booleanMatrix) {
+      output += row.map((b) => (b ? "1" : ".")).join("") + "\n";
+    }
+    return output;
+  }
+
+  beforeAll(() => {
+    configure({
+      reporters: [new JestReporter()],
+    });
+  });
+
+  it("should print", () => {
+    const booleanMatrix: boolean[][] = [
+      [true, false, false, false],
+      [false, true, false, false],
+      [false, false, true, false],
+      [false, false, false, false, false],
+      [true, true, true, true, true],
+    ];
+    verify(printBooleanMatrix(booleanMatrix));
   });
 });
