edfdb29a924cf7bfde6f1f7081fa8f58ffac583f
Stefan Boos <kontakt@boos.systems>
2023-12-22 13:38:03 +0100

    docs(setup-next-day): describe idea and solution approach

--- src/setup-next-day.ts
+++ src/setup-next-day.ts
@@ -1,12 +1,43 @@
 const argv = require("minimist")(process.argv.slice(2));
 
+// Idea
+// ====
+//
+// WHEN I run ts-node ./setup-next-day.ts 12 example,
+// THEN The following files shall be generated from the templates:
+// - ./src/12-example.ts
+// - ./tests/12-example.test.ts
+// - ./inputs/12-example.txt
+// - ./inputs/12-example-from-puzzle-description.txt
+//
+// Solution
+// --------
+//
+// CopyData is a structure of
+// - folder: path
+// - template: filename
+// - destination: filename
+//
+// Destination file is derived from day and puzzleName arguments using the constructor
+//
+// For each entry in CopyData:
+//   Copy folder/template to folder/destination
+//
+// Solution approach
+// -----------------
+//
+// Start with printing the instructions instead of copying
+// Make the target folder a parameter of the test
+// Create temporary directory in the test for an integration test
+//
+
 export const UsageInstructions = `USAGE: ts-node setup-next-day.ts DAY PUZZLENAME
 
 Copy the template files to the next puzzle.
 
-  DAY        is the number of the advent of code day to set up.
+  DAY          is the number of the advent of code day to set up.
 
-  PUZZLENAME is a shorthand inserted into the filename to give a clue about the puzzle topic.`;
+  PUZZLENAME   is a shorthand inserted into the filename to give a clue about the puzzle topic.`;
 
 export function setupNextDay(
   day: number | undefined,
@@ -17,7 +48,7 @@ export function setupNextDay(
     return;
   }
 
-  console.log(`Copying ./src/template.ts to ./src/${day}-${puzzleName}.ts`);
+  console.log(`Copy ./src/template.ts to ./src/${day}-${puzzleName}.ts`);
 }
 
 setupNextDay(argv._[0], argv._[1]);
--- tests/setup-next-day.test.ts
+++ tests/setup-next-day.test.ts
@@ -8,28 +8,19 @@ it.each([
   const logSpy = jest.spyOn(global.console, "log").mockImplementation(() => {});
   setupNextDay(day, puzzleName);
 
-  expect(logSpy).toHaveBeenCalledWith(
-    `Copying ./src/template.ts to ${expected}`,
-  );
+  expect(logSpy).toHaveBeenCalledWith(`Copy ./src/template.ts to ${expected}`);
   logSpy.mockRestore();
 });
 
 describe("reports parameter errors and does not copy", () => {
-  function containsCopyMessage(mockCalls: string[][]) {
-    const loggedCopyMessages = mockCalls.filter((call) =>
-      call[0].startsWith("Copying"),
-    );
-    return loggedCopyMessages.length > 0;
-  }
-
   it("when day is undefined", () => {
     const logSpy = jest
       .spyOn(global.console, "log")
       .mockImplementation(() => {});
     setupNextDay(undefined, "this text is ignored");
 
+    expect(logSpy).toHaveBeenCalledTimes(1);
     expect(logSpy).toHaveBeenCalledWith(UsageInstructions);
-    expect(containsCopyMessage(logSpy.mock.calls)).toBeFalsy();
     logSpy.mockRestore();
   });
 
@@ -39,8 +30,8 @@ describe("reports parameter errors and does not copy", () => {
       .mockImplementation(() => {});
     setupNextDay(10, undefined);
 
+    expect(logSpy).toHaveBeenCalledTimes(1);
     expect(logSpy).toHaveBeenCalledWith(UsageInstructions);
-    expect(containsCopyMessage(logSpy.mock.calls)).toBeFalsy();
 
     logSpy.mockRestore();
   });
