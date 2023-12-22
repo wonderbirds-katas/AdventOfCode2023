058e5dbe714f41e5a4d04f49ea23b67719734cd5
Stefan Boos <kontakt@boos.systems>
2023-12-22 15:47:34 +0100

    feat(setup-next-day): allow overriding parent target directory

--- tests/setup-next-day.test.ts
+++ tests/setup-next-day.test.ts
@@ -3,6 +3,9 @@ import {
   setupNextDay,
   UsageInstructions,
 } from "../src/setup-next-day";
+import { mkdirSync, mkdtempSync, rmSync } from "node:fs";
+import * as path from "node:path";
+import { tmpdir } from "node:os";
 import SpyInstance = jest.SpyInstance;
 
 describe("setup-next-day should", () => {
@@ -22,16 +25,34 @@ describe("setup-next-day should", () => {
     ["src/42-puzzle-name.ts", 42, "puzzle-name"],
     ["src/10-other-name.ts", 10, "other-name"],
   ])("copy src/template.ts to %s", (expected, day, puzzleName) => {
-    setupNextDay(day, puzzleName, new CopyCommandPrinter());
+    setupNextDay(day, puzzleName, "", new CopyCommandPrinter());
 
     expect(logSpy).toHaveBeenCalledWith(`Copy src/template.ts to ${expected}`);
   });
 
   it.each([
+    [
+      "/target-parent-dir/src/08-puzzle-name.ts",
+      8,
+      "puzzle-name",
+      "/target-parent-dir",
+    ],
+  ])(
+    "copy src/template.ts to %s",
+    (expected, day, puzzleName, targetParentDir) => {
+      setupNextDay(day, puzzleName, targetParentDir, new CopyCommandPrinter());
+
+      expect(logSpy).toHaveBeenCalledWith(
+        `Copy src/template.ts to ${expected}`,
+      );
+    },
+  );
+
+  it.each([
     ["test/08-puzzle-name.test.ts", 8, "puzzle-name"],
     ["test/10-other-name.test.ts", 10, "other-name"],
   ])("copy test/template.test.ts to %s", (expected, day, puzzleName) => {
-    setupNextDay(day, puzzleName, new CopyCommandPrinter());
+    setupNextDay(day, puzzleName, "", new CopyCommandPrinter());
 
     expect(logSpy).toHaveBeenCalledWith(
       `Copy test/template.test.ts to ${expected}`,
@@ -40,17 +61,44 @@ describe("setup-next-day should", () => {
 
   describe("not copy but report parameter errors", () => {
     it("when day is undefined", () => {
-      setupNextDay(undefined, "this text is ignored", new CopyCommandPrinter());
+      setupNextDay(
+        undefined,
+        "this text is ignored",
+        "",
+        new CopyCommandPrinter(),
+      );
 
       expect(logSpy).toHaveBeenCalledTimes(1);
       expect(logSpy).toHaveBeenCalledWith(UsageInstructions);
     });
 
     it("when puzzle name is undefined", () => {
-      setupNextDay(10, undefined, new CopyCommandPrinter());
+      setupNextDay(10, undefined, "", new CopyCommandPrinter());
 
       expect(logSpy).toHaveBeenCalledTimes(1);
       expect(logSpy).toHaveBeenCalledWith(UsageInstructions);
     });
+  });
+});
+
+describe("setup-next-day integration test", () => {
+  it("should copy files to destination folder", () => {
+    const prefix = "setup-next-day-tests-";
+
+    let tempDir = "";
+    try {
+      tempDir = mkdtempSync(path.join(tmpdir(), prefix));
+      console.log(`Created temporary directory "${tempDir}"`);
+
+      mkdirSync(path.join(tempDir, "src"));
+      mkdirSync(path.join(tempDir, "test"));
+
+      setupNextDay(10, "puzzle-name", tempDir, new CopyCommandPrinter());
+    } finally {
+      if (tempDir) {
+        rmSync(tempDir, { recursive: true });
+        console.log(`Deleted temporary directory "${tempDir}"`);
+      }
+    }
   });
 });
