55dd4c932f6458b307d0ebc8badb31f40b65ef9a
Stefan Boos <kontakt@boos.systems>
2023-12-22 08:36:41 +0100

    feat(setup-next-day): validate command line arguments

--- src/setup-next-day.ts
+++ src/setup-next-day.ts
@@ -1,7 +1,23 @@
 const argv = require("minimist")(process.argv.slice(2));
 
-export function setupNextDay(day: number) {
-  console.log(`Setting up day ${day}`);
+export const UsageInstructions = `USAGE: ts-node setup-next-day.ts DAY PUZZLENAME
+
+Copy the template files to the next puzzle.
+
+  DAY        is the number of the advent of code day to set up.
+
+  PUZZLENAME is a shorthand inserted into the filename to give a clue about the puzzle topic.`;
+
+export function setupNextDay(
+  day: number | undefined,
+  puzzleName: string | undefined,
+) {
+  if (day === undefined || puzzleName === undefined) {
+    console.log(UsageInstructions);
+    return;
+  }
+
+  console.log(`Copying ./src/template.ts to ./src/${day}-${puzzleName}.ts`);
 }
 
-setupNextDay(argv._[0]);
+setupNextDay(argv._[0], argv._[1]);
