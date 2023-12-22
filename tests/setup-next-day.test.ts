c63e80bab1c998f27831cd0dd6b89f6c171fd8f4
Stefan Boos <kontakt@boos.systems>
2023-12-22 14:29:55 +0100

    refactor(setup-next-day): design algorithm skeleton

--- tests/setup-next-day.test.ts
+++ tests/setup-next-day.test.ts
@@ -1,38 +1,46 @@
-import { setupNextDay, UsageInstructions } from "../src/setup-next-day";
+import {
+  CopyCommandPrinter,
+  setupNextDay,
+  UsageInstructions,
+} from "../src/setup-next-day";
 
-it.each([
-  ["./src/12-puzzle-name.ts", 12, "puzzle-name"],
-  ["./src/42-puzzle-name.ts", 42, "puzzle-name"],
-  ["./src/10-other-name.ts", 10, "other-name"],
-])("copies src/template.ts to %s", (expected, day, puzzleName) => {
-  const logSpy = jest.spyOn(global.console, "log").mockImplementation(() => {});
-  setupNextDay(day, puzzleName);
-
-  expect(logSpy).toHaveBeenCalledWith(`Copy ./src/template.ts to ${expected}`);
-  logSpy.mockRestore();
-});
-
-describe("reports parameter errors and does not copy", () => {
-  it("when day is undefined", () => {
+describe("setup-next-day should", () => {
+  it.each([
+    ["src/12-puzzle-name.ts", 12, "puzzle-name"],
+    ["src/42-puzzle-name.ts", 42, "puzzle-name"],
+    ["src/10-other-name.ts", 10, "other-name"],
+  ])("copy src/template.ts to %s", (expected, day, puzzleName) => {
     const logSpy = jest
       .spyOn(global.console, "log")
       .mockImplementation(() => {});
-    setupNextDay(undefined, "this text is ignored");
+    setupNextDay(day, puzzleName, new CopyCommandPrinter());
 
-    expect(logSpy).toHaveBeenCalledTimes(1);
-    expect(logSpy).toHaveBeenCalledWith(UsageInstructions);
+    expect(logSpy).toHaveBeenCalledWith(`Copy src/template.ts to ${expected}`);
     logSpy.mockRestore();
   });
 
-  it("when puzzle name is undefined", () => {
-    const logSpy = jest
-      .spyOn(global.console, "log")
-      .mockImplementation(() => {});
-    setupNextDay(10, undefined);
+  describe("not copy but report parameter errors", () => {
+    it("when day is undefined", () => {
+      const logSpy = jest
+        .spyOn(global.console, "log")
+        .mockImplementation(() => {});
+      setupNextDay(undefined, "this text is ignored", new CopyCommandPrinter());
 
-    expect(logSpy).toHaveBeenCalledTimes(1);
-    expect(logSpy).toHaveBeenCalledWith(UsageInstructions);
+      expect(logSpy).toHaveBeenCalledTimes(1);
+      expect(logSpy).toHaveBeenCalledWith(UsageInstructions);
+      logSpy.mockRestore();
+    });
 
-    logSpy.mockRestore();
+    it("when puzzle name is undefined", () => {
+      const logSpy = jest
+        .spyOn(global.console, "log")
+        .mockImplementation(() => {});
+      setupNextDay(10, undefined, new CopyCommandPrinter());
+
+      expect(logSpy).toHaveBeenCalledTimes(1);
+      expect(logSpy).toHaveBeenCalledWith(UsageInstructions);
+
+      logSpy.mockRestore();
+    });
   });
 });
