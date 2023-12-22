257bd0f8681be713536c1f358238ca581a0a27fd
Stefan Boos <kontakt@boos.systems>
2023-12-22 17:26:29 +0100

    feat(setup-next-day): copy input files as well

--- src/setup-next-day.ts
+++ src/setup-next-day.ts
@@ -3,33 +3,9 @@ import { copyFileSync } from "node:fs";
 
 const argv = require("minimist")(process.argv.slice(2));
 
-// Idea
-// ====
-//
-// WHEN I run ts-node ./setup-next-day.ts 12 example,
-// THEN The following files shall be generated from the templates:
-// - ./src/12-example.ts
-// - ./tests/12-example.test.ts
-// - ./inputs/12-example.txt
-// - ./inputs/12-example-from-puzzle-description.txt
-//
-// Solution
-// --------
-//
-// Destination file is derived from day and puzzleName arguments using the constructor
-//
-// For each entry in CopyData:
-//   Copy folder/template to folder/destination
-//
-// Solution approach
-// -----------------
-//
-// Start with printing the instructions instead of copying
-// Make the target folder a parameter of the test
-// Create temporary directory in the test for an integration test
-//
-
-export const UsageInstructions = `USAGE: ts-node ./setup-next-day.ts DAY PUZZLENAME
+// prettier-ignore
+export const UsageInstructions =
+`USAGE: ts-node ./src/setup-next-day.ts DAY PUZZLENAME
 
 Copy the template files to the next puzzle.
 
@@ -48,16 +24,12 @@ export function setupNextDay(
     return;
   }
 
+  // prettier-ignore
   const templateFiles = [
     new TemplateFile("src", "template.ts", day, puzzleName, targetParentDir),
-    new TemplateFile(
-      "tests",
-      "template.test.ts",
-      day,
-      puzzleName,
-      targetParentDir,
-      ".test",
-    ),
+    new TemplateFile("tests", "template.test.ts", day, puzzleName, targetParentDir, ".test"),
+    new TemplateFile("inputs", "template.txt", day, puzzleName, targetParentDir),
+    new TemplateFile("inputs", "template-from-puzzle-description.txt", day, puzzleName, targetParentDir, "-from-puzzle-description"),
   ];
 
   for (const templateFile of templateFiles) {
@@ -69,11 +41,13 @@ export function setupNextDay(
 class TemplateFile {
   private readonly _destinationFile: string;
 
-  // folder: path to the folder containing the template and receiving the copy
-  // sourceFile: filename of the template
-  // day: number of advent of code day
-  // puzzleName: short name to distinguish the name of the puzzle
-  // suffix: file name suffix to insert between puzzleName and extension
+  //          folder: path to the folder containing the template and receiving the copy
+  //      sourceFile: filename of the template
+  //             day: number of advent of code day
+  //      puzzleName: short name to distinguish the name of the puzzle
+  // targetParentDir: directory below which the copied files shall be placed.
+  //                  Allows integration tests to copy to a temporary directory.
+  //          suffix: file name suffix to insert between puzzleName and extension
   constructor(
     readonly folder: string,
     readonly sourceFile: string,
@@ -83,7 +57,12 @@ class TemplateFile {
     readonly suffix: string = "",
   ) {
     const zeroPaddedDay = `${day}`.padStart(2, "0");
-    this._destinationFile = `${zeroPaddedDay}-${this.puzzleName}${suffix}.ts`;
+
+    const extensionStart = sourceFile.lastIndexOf(".");
+    const extension =
+      extensionStart === -1 ? "" : sourceFile.substring(extensionStart);
+
+    this._destinationFile = `${zeroPaddedDay}-${this.puzzleName}${suffix}${extension}`;
   }
 
   get sourcePath() {
